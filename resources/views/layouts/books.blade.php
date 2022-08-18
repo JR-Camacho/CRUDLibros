87678c5d333ca3a47330712be5ca0eaca7f18059
JR-Camacho <josecamachoc0303@gmailcom>
2022-08-18 13:47:22 -0400

    Cambio de Titulo y Favicon

--- resources/views/layouts/books.blade.php
+++ resources/views/layouts/books.blade.php
@@ -8,8 +8,9 @@
     <link rel="stylesheet" href="{{url('favicon.png')}}" rel="icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="shortcut icon" href="{{ asset('favicon.png') }}">
     <link rel="stylesheet" href="{{secure_asset('/css/styles.css')}}">
-    <title>BooksForYou</title>
+    <title>LibraryForYou</title>
 </head>
 
 <body>
